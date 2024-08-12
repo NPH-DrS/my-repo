3c64a9b87bd68ded9918ea0359bec21ce73220b1
Nguyễn Phúc Hiệp <132082516+DraiGShyva@users.noreply.github.com>
2024-08-12 11:04:21 +0700

    update detail.js cosplaytele

--- cosplaytele/src/detail.js
+++ cosplaytele/src/detail.js
@@ -3,18 +3,32 @@ function execute(url) {
     let response = fetch(url);
     if (response.ok) {
         let doc = response.html();
-        
-         const contentElement = doc.select("blockquote"); // Assuming the content is within a blockquote element
 
-         let stt = "";
-         const photosLine = contentElement.select("p:nth-child(2) strong:nth-child(1)").text().trim();
-         stt += `${photosLine}.`;
-        
+        const contentElement = doc.select("blockquote");
+
+        let stt = "";
+
+        const cosplayerLink = contentElement.select("p.gt-block strong a").attr("href");
+        const cosplayerName = contentElement.select("p.gt-block strong a").text().trim();
+        stt += `Cosplayer: ${cosplayerName} <br>(Link: ${cosplayerLink})`;
+
+        const appearInLink = contentElement.select("p.gt-block strong:nth-child(3) a").attr("href");  // Optional for "Appear In" field
+        const appearIn = contentElement.select("p.gt-block strong:nth-child(3)").text().trim();
+        if (appearIn) {
+            stt += `<br>Appear In: ${appearIn} <br>(Link: ${appearInLink})\n`;  // Include link if available
+        }
+
+        const photosLine = contentElement.select("p:nth-child(2) strong:nth-child(1)").text().trim();
+        stt += `<br>${photosLine}.`;
+
+        const fileSize = contentElement.select("p:nth-child(2) strong:nth-child(2)").text().trim();
+        stt += `File Size: ${fileSize}\n`;
+
         return Response.success({
             name: doc.select("title").first().text(),
             cover: doc.select('meta[property="og:image"]').first().attr("content"),
             author: 'CosplayTele',
-            description: stt,
+            detail: stt,
             host: BASE_URL,
         });
     }
