252d6a214977f2a5cff4c3e69db5ae71b12f933e
Nguyễn Phúc Hiệp <132082516+DraiGShyva@users.noreply.github.com>
2024-08-12 13:34:17 +0700

    update detail.js cosplaytele

--- cosplaytele/src/detail.js
+++ cosplaytele/src/detail.js
@@ -4,31 +4,28 @@ function execute(url) {
     if (response.ok) {
         let doc = response.html();
 
+        let genres = [];
+        doc.select(".entry-category a").forEach(e => {
+            genres.push({
+                title: e.text(),
+                input: e.attr("href"),
+                script: "gen.js"
+            });
+        });
+
         const contentElement = doc.select("blockquote");
-
-        let stt = "";
-
-        const cosplayerLink = contentElement.select("p strong a").attr("href");
         const cosplayerName = contentElement.select("p strong a").text().trim();
-        stt += `Cosplayer: ${cosplayerName} <br>(Link: ${cosplayerLink})`;
-
-        const appearInLink = contentElement.select("p strong:nth-child(3) a").attr("href");  // Optional for "Appear In" field
-        const appearIn = contentElement.select("p strong:nth-child(3)").text().trim();
-        if (appearIn) {
-            stt += `<br>Appear In: ${appearIn} <br>(Link: ${appearInLink})\n`;  // Include link if available
-        }
-
-        const photosLine = contentElement.select("p:nth-child(2) strong:nth-child(1)").text().trim();
-        stt += `<br>${photosLine}.`;
-
-        const fileSize = contentElement.select("p:nth-child(2) strong:nth-child(2)").text().trim();
-        stt += `File Size: ${fileSize}\n`;
+        const characterName = contentElement.select("p strong:nth-child(3)").text().trim();
+        const photosLine = contentElement.select("p:nth-child(2) strong").text().trim();
+        const fileSize = contentElement.select("p:nth-child(2) strong:nth-child(3)").text().trim();
 
         return Response.success({
             name: doc.select("title").first().text(),
             cover: doc.select('meta[property="og:image"]').first().attr("content"),
-            author: 'CosplayTele',
-            detail: stt,
+            author: contentElement.select("p strong:nth-child(5) a").text().trim(),
+            genres,
+            detail: `Cosplayer: ${cosplayerName})<br>${characterName}<br>${photosLine}<br>File Size: ${fileSize}`,
+            description: doc.select('meta[property="og:image"]').first().attr("content"),
             host: BASE_URL,
         });
     }
